66330327db58d706cdcc8b8639cdeee2d5c07f62
Kiana <77738487+Kawaiio22@users.noreply.github.com>
2025-12-23 14:33:34 -0800

    Update servo_test.py
    Testing each servo while adjusting angles, starting with a smaller values instead of the extremes

--- servo_test.py
+++ servo_test.py
@@ -2,13 +2,15 @@ from adafruit_servokit import ServoKit
 import time
 #16 channels PCA9685 board
 kit = ServoKit(channels = 16)
-kit.servo[0].angle=0 #middle
-kit.servo[3].angle = 0 
+kit.servo[0].angle= 0 #middle
+kit.servo[3].angle = 70
 print("hi")
+
+
+
 
 time.sleep(1)
 kit.servo[0].angle = 180
-kit.servo[3].angle = 180
+kit.servo[3].angle = 90
 print("hi again")
-
 
